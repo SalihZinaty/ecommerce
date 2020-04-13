1c4f618e3ae3f46723ac5568b525e9848a3d0e8c
SalihZinaty <salihzinaty@gmail.com>
2020-04-13 18:28:56 +0300

    intigrated with firebase and sign up

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shop/ShopPage';
 import Header from './components/Header/Header';
 import SignsPage from './pages/SignsPage/SignsPage';
 
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(){
@@ -18,10 +18,22 @@ class App extends React.Component {
   }
   unsubscribeFromAuth = null;
   componentDidMount() {
-   this.unsubscribeFromAuth =  auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user})
-      console.log(user)
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
 
   componentWillUnmount() {
--- src/pages/SignsPage/SignsPage.js
+++ src/pages/SignsPage/SignsPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './SignsPage.scss';
 import SignIn from '../../components/SignIn/SignIn';
+import SignUp from '../../components/SignUp/SignUp';
 const SignsPage = () => (
     <div className='signs-page'>
          <SignIn/>
+         <SignUp/>
     </div>
 )
 
